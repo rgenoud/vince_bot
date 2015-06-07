cd39f47902bb2d3d59ecefb92f935d73a0f99227
Richard Genoud <richard.genoud@gmail.com>
2015-06-07 16:57:48 +0200

    ajout du script pour chercher des blagues

---
+++ blague.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget -t 1 -T 5 "http://humour-blague.com/blagues-2/index.php#" -O- 2>/dev/null | sed -e '1,/                        <p align="left" class="blague">/ d' -e '/                        <p align="left" class="blague">&nbsp;<\/p>/,$ d' 2>/dev/null | html2text | sed -e 's/&quot;/"/g'
